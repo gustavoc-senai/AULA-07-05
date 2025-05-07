3dca2f49645e988c4a56a6dc227a5b2a2d9bfdb4
gustavoc-senai <carvalhogustavo944@gmail.com>
2025-05-07 14:06:04 -0300

    Carvalho

--- Exemplo.java
+++ Exemplo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exemplo {
 	public static void main(String[] args) {
-		
+	
 		int a[] = new int [5];
 		Scanner ler = new Scanner (System.in);
 		for (int i =0; i<5; i++) {
