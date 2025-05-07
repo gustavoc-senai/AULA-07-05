0734251ec806993d23bd235c8828b58f13f0f8ac
unknown <carvalhogustavo944@gmail.com>
2025-05-07 13:47:48 -0300

    Carvalho

---
+++ INT.java
@@ -0,0 +1,13 @@
+package Aula0705;
+
+public class INT {
+	public static void main(String[] args) {
+		int[] numeros = new int[5]; // vetor de 5 posições 
+		numeros[0] = 10; 
+		numeros[1] = 20; 
+		numeros[2] = 30; 
+		numeros[3] = 40;
+		numeros[4] = 50;
+		System.out.println(numeros[3]); 
+	}
+}
---
+++ STRING.java
@@ -0,0 +1,14 @@
+package Aula0705;
+
+public class STRING {
+	public static void main(String[] args) {
+		String[] nomes = new String[5]; 
+		nomes[0] = "Renzo"; 
+		nomes[1] = "Enzo"; 
+		nomes[2] = "Samuel"; 
+		nomes[3] = "Gustavo";
+		nomes[4] = "João";
+		System.out.println(nomes[1]);
+		 
+	}
+}
