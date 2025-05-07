87bf7351186f54b1a842a446083a8f1ad6a02ccf
gustavoc-senai <carvalhogustavo944@gmail.com>
2025-05-07 14:04:37 -0300

    Update ATIV1.java

--- ATIV1.java
+++ ATIV1.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class ATIV1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // para ler entrada do usuário
+        Scanner scanner = new Scanner(System.in);
 
-        String[] nomes = new String[5]; // vetor para armazenar 5 nomes
+        String[] nomes = new String[5]; 
 
-        // Leitura dos nomes
+       
         for (int i = 0; i < 5; i++) {
             System.out.print("Digite o nome " + (i + 1) + ": ");
-            nomes[i] = scanner.nextLine(); // lê o nome e armazena no vetor
+            nomes[i] = scanner.nextLine(); 
         }
 
-        // Exibição dos nomes armazenados
+        
         System.out.println("\nNomes digitados:");
         for (int i = 0; i < 5; i++) {
             System.out.println("Nome " + (i + 1) + ": " + nomes[i]);
         }
 
-        scanner.close(); // fecha o Scanner
+        scanner.close(); 
     }
 }
